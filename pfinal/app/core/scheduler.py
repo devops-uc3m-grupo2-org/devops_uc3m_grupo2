2cae72a3edffb8c26db9067925ed5b1a6a77f82a
100485446-UC3M <100485446@alumnos.uc3m.es>
2026-04-08 13:11:18 +0200

    feat/test/doc: MatchAlert Funcional con respectivos test y documentación sprint3

--- pfinal/app/core/scheduler.py
+++ pfinal/app/core/scheduler.py
@@ -1,7 +1,9 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from app.core.database import SessionLocal
-from app.models.models import InformationSource
+from app.models.models import InformationSource, NewsItem, Alert, AlertNews
 from app.services.fetcher import fetch_feed
+from app.services.notifications import notify_user
+from app.services.alertLogic import match_alert
 
 scheduler = BackgroundScheduler()
 
@@ -14,20 +16,41 @@ def fetch_all_sources_job():
 
         for source in sources:
             try:
-                new_items = fetch_feed(db, source.id, limit=10)
-                print(f"[FETCH] Source {source.id}: {new_items} new items")
+                n_new_items = fetch_feed(db, source.id, limit=10) #Limitado a 10 par efectos de debugging
+                items = db.query(NewsItem).order_by(NewsItem.id.desc()).limit(n_new_items).all() #Los últimos introducidos
+
+                process_alerts_for_items(db, items)
+
+                print(f"[FETCH] Source {source.id}: {n_new_items} new items")
             except Exception as e:
                 print(f"[ERROR] Source {source.id}: {e}")
 
     finally:
         db.close()
 
+def process_alerts_for_items(db, items):
+    alerts = db.query(Alert).filter(Alert.is_active == True).all()
+
+    for item in items:
+        for alert in alerts:
+            if match_alert(alert, item):
+                print(f"[MATCH] Alert {alert.id} matched News {item.id}")
+                exists = db.query(AlertNews).filter_by(
+                        alert_id = alert.id,
+                        news_item_id = item.id).first()
+
+                if not exists:
+                    db.add(AlertNews(
+                        alert_id=alert.id,
+                        news_item_id=item.id))
+                notify_user(alert)
+
 
 def start_scheduler():
     scheduler.add_job(
         fetch_all_sources_job,
-        "interval",
-        minutes=5,
+        trigger="cron",
+        minute="*/5",
         max_instances=1
     )
 
